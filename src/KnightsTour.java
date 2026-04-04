a76c67abec537e6544dc22a7421fab82ac9e597a
Mohammad Arshan Shaikh <shaikh.mohammada@northeastern.edu>
2026-04-04 16:42:28 -0400

    Implement Warnsdorff heuristic solver

--- src/KnightsTour.java
+++ src/KnightsTour.java
@@ -18,4 +18,33 @@ public class KnightsTour {
                 count++;
         return count;
     }
+
+    static int[] warnsdorff(boolean[][] visited) {
+        int[] tour = new int[ROWS * COLS];
+        int r = 0, c = 0;
+        visited[r][c] = true;
+        tour[0] = r * COLS + c;
+
+        for (int step = 1; step < ROWS * COLS; step++) {
+            int bestR = -1, bestC = -1, bestDeg = Integer.MAX_VALUE;
+            for (int[] m : MOVES) {
+                int nr = r + m[0], nc = c + m[1];
+                if (inBounds(nr, nc) && !visited[nr][nc]) {
+                    int d = degree(nr, nc, visited);
+                    if (d < bestDeg || (d == bestDeg && nr * COLS + nc < bestR * COLS + bestC)) {
+                        bestDeg = d;
+                        bestR = nr;
+                        bestC = nc;
+                    }
+                }
+            }
+            if (bestR == -1)
+                return null;
+            visited[bestR][bestC] = true;
+            tour[step] = bestR * COLS + bestC;
+            r = bestR;
+            c = bestC;
+        }
+        return tour;
+    }
 }
