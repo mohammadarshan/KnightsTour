841bcc9599428a68a89e17a4ded92e3c044c17ec
Mohammad Arshan Shaikh <shaikh.mohammada@northeastern.edu>
2026-04-04 16:31:21 -0400

    Board constants and class skeleton

---
+++ src/KnightsTour.java
@@ -0,0 +1,9 @@
+public class KnightsTour {
+
+    static final int[][] MOVES = {
+            { -2, -1 }, { -2, 1 }, { -1, -2 }, { -1, 2 },
+            { 1, -2 }, { 1, 2 }, { 2, -1 }, { 2, 1 }
+    };
+
+    static int ROWS, COLS;
+}
