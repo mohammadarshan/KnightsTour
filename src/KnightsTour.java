5a63e66d1defe09f5159c2d0eb43ce78daffd6b5
Mohammad Arshan Shaikh <shaikh.mohammada@northeastern.edu>
2026-04-04 16:33:08 -0400

    add move generation and degree helper

--- src/KnightsTour.java
+++ src/KnightsTour.java
@@ -6,4 +6,16 @@ public class KnightsTour {
     };
 
     static int ROWS, COLS;
+
+    static boolean inBounds(int r, int c) {
+        return r >= 0 && r < ROWS && c >= 0 && c < COLS;
+    }
+
+    static int degree(int r, int c, boolean[][] visited) {
+        int count = 0;
+        for (int[] m : MOVES)
+            if (inBounds(r + m[0], c + m[1]) && !visited[r + m[0]][c + m[1]])
+                count++;
+        return count;
+    }
 }
